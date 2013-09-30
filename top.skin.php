6246904d6f1c7cb0af359f6a21dd84aafbaf9cf3
hokuken <customer@hokuken.com>
2013-09-30 18:15:28 +0900

    license へ統一

--- top.skin.php
+++ top.skin.php
@@ -71,8 +71,8 @@
 	<div class="container">
 		#{$site_footer}
 	</div>
-	<div id="licence" class="container">
-		#{$licence_tag}
+	<div id="license" class="container">
+		#{$license_tag}
 	</div>
 </footer>
 
