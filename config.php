b02e9e67e585c6a6b582f0cf95a7a7fa3911c587
rmasamori <r.masamori@gmail.com>
2014-02-14 15:07:03 +0900

    v1.1.1

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array(
 	'name' => 'semi',
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'thumbnail' => 'img/thumbnail.png',
 	'style_file' => 'css/docs.css',
 	'templates' => array(
